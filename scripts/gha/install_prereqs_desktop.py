ff2a2ac3ebdeb47493fc15e825215748c31d3780
Jon Simantov <jsimantov@google.com>
2021-03-10 12:30:55 -0800

    Add --openssl option to prereqs script to install openssl if needed.

--- scripts/gha/install_prereqs_desktop.py
+++ scripts/gha/install_prereqs_desktop.py
@@ -14,24 +14,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-This script needs to be run once to prepare the machine for building SDK.
-It will download required python dependencies and also install ccache on mac/linux.
-ccache considerably improves the build times.
+"""Install prerequisites for desktop build.
+
+This script needs to be run once to prepare the machine for building SDK.  It
+will download required python dependencies and also install ccache on mac/linux.
+ccache considerably improves the build times. OpenSSL will only be installed
+ifrequested.
 
 Please note that this script is aganostic of various desktop configurations.
 For example, you can run it once regardless if you are following up with a build of x86 or x64.
 
-Run this script from the root of the repository
+Run this script from the root of the repository.
 
 Usage:
-python scripts/gha/install_prereqs_desktop.py
+python scripts/gha/install_prereqs_desktop.py [--openssl]
 
 """
 
+import getopt
+import sys
 import utils
 
+def usage():
+  print("""Usage: %s [--openssl]
+    --openssl: Install openssl, not installed by default.
+""" % sys.argv[0])
+
+
 def main():
+  option_install_openssl = False
+
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], 'h', ['help', 'openssl'])
+  except getopt.error as err:
+    print(str(err))
+    usage()
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+      usage()
+      sys.exit(0)
+    if opt in ('--openssl'):
+      option_install_openssl = True
+  
   # Install protobuf on linux/mac if its not installed already
   if not utils.is_command_installed('protoc'):
     if utils.is_linux_os():
@@ -50,8 +76,8 @@ def main():
         # brew install protobuf
         utils.run_command(['brew', 'install', 'go'])
 
-  # Install openssl on linux/mac if its not installed already
-  if not utils.is_command_installed('go'):
+  # Install openssl on linux/mac if it's requested
+  if option_install_openssl:
     if utils.is_linux_os():
         # sudo apt install -y openssl
         utils.run_command(['apt', 'install', '-y','openssl'], as_root=True)
